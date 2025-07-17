4eb9bf9498040a755e1ebb8aea990691c6175261
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-17 03:04:10 -0300

    chore(msw): add comments to exemplify msw usage

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,6 +1,8 @@
 import { setupWorker } from 'msw/browser';
 import { env } from '../../env/config';
 
+// passa os mocks dentro dessa função
+// Exemplo: setupWorker(SignInMock, CreateUserMock)
 export const worker = setupWorker();
 
 export const enableMSW = async () => {
