b47dd3ec29fabfa879d5fe3cfd7caa1d23ca9aed
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-16 11:48:42 -0300

    fix(main): update import paths for AppProvider and MSW mocks

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { AppProvider } from '@/context/app-provider.tsx';
+import { enableMSW } from '@/services/api/mocks/index.ts';
 import { App } from './App.tsx';
-import { enableMSW } from './api/mocks/index.ts';
-import { AppProvider } from './context/app-provider.tsx';
 
 enableMSW().then(() => {
   // biome-ignore lint/style/noNonNullAssertion: mandatory by React
