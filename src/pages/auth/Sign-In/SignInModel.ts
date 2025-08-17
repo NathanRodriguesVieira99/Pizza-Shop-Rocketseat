189252c63ee3124400645557babacb7b5575160d
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-16 23:13:36 -0300

    feat(SignInModel): add and export initial React Hook Form logic

--- src/pages/auth/Sign-In/SignInModel.ts
+++ src/pages/auth/Sign-In/SignInModel.ts
@@ -1,3 +1,20 @@
+import { useForm } from 'react-hook-form';
+import type { SignFormSchema } from './SignIn-schema';
+
 export const useSignInModel = () => {
-  return {};
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignFormSchema>();
+
+  const onSubmit = async (data: SignFormSchema) => {
+    console.log(data);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  };
+
+  const handleSignIn = handleSubmit(onSubmit);
+
+  return { register, handleSignIn, isSubmitting };
 };
