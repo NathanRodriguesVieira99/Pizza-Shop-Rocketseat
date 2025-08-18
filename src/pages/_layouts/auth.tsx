043026cd404b5a4680780f6196dccd3c9f4cbfcc
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-18 20:43:58 -0300

    style(layout): update AuthLayout styles

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,17 +3,19 @@ import { Outlet } from 'react-router';
 
 export const AuthLayout = () => {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+    <div className="grid min-h-screen grid-cols-2 antialiased">
       <div className="flex h-full flex-col justify-between border-foreground/5 border-r bg-muted p-10 text-muted-foreground">
         <div className=" flex items-center gap-3 text-foreground text-lg">
           <Pizza className="h-5 w-5" />
           <span className="font-semibold">pizza.shop</span>
         </div>
+
         <footer className="text-sm">
           Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
       </div>
-      <div className="flex flex-col items-center justify-center">
+
+      <div className="relative flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
