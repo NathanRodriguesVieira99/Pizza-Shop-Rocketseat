ecd249353e715db8e7f0e5d56d0e299f1c47cfed
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-18 20:42:18 -0300

    feat(provider): add sonner ToastProvider to AppProvider

--- src/context/app-provider.tsx
+++ src/context/app-provider.tsx
@@ -1,11 +1,14 @@
 import type { ReactNode } from 'react';
 import { QueryClientProviders } from './providers/query-client-provider';
 import { ReactHelmetProvider } from './providers/react-helmet-async-provider';
+import { ToastProvider } from './providers/toast-provider';
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProviders>
-      <ReactHelmetProvider>{children}</ReactHelmetProvider>
+      <ReactHelmetProvider>
+        <ToastProvider>{children}</ToastProvider>
+      </ReactHelmetProvider>
     </QueryClientProviders>
   );
 };
