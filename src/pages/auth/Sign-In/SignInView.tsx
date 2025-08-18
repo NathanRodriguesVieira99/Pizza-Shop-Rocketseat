4230af12cc9388507d13eac59b54c7485b0009a8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-18 20:46:20 -0300

    feat(auth): add Link to SignUp page

--- src/pages/auth/Sign-In/SignInView.tsx
+++ src/pages/auth/Sign-In/SignInView.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -12,14 +13,18 @@ export const SignInView = (props: SignInViewProps) => {
   return (
     <>
       <Helmet title="Login" />
-      <div className="p-8 ">
+      <div className="p-8">
+        <Button asChild className="absolute top-8 right-8" variant={'ghost'}>
+          <Link to="/signUp">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="font-semibold text-2xl tracking-tight">
               Acessar Painel
             </h1>
             <p className="text-muted-foreground text-sm">
-              Acompanhe suas vendas pelo painel do parceiro{' '}
+              Acompanhe suas vendas pelo painel do parceiro
             </p>
           </div>
 
