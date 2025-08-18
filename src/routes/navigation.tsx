b2636f8c22fd73f9014fb96e666ef56fb1bb5a1d
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-18 20:47:08 -0300

    feat(navigation): add SignUp route

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -3,9 +3,11 @@ import { AppLayout } from '@/pages/_layouts/app';
 import { AuthLayout } from '@/pages/_layouts/auth';
 import { DashboardPage } from '@/pages/app/Dashboard/DashboardPage';
 import { SignInPage } from '@/pages/auth/Sign-In/SignInPage';
+import { SignUpPage } from '@/pages/auth/SignUp/SignUpPage';
 
 export const RoutesPath = {
   signIn: '/signIn',
+  signUp: '/signUp',
 } as const;
 
 export const Navigation = () => {
@@ -18,6 +20,7 @@ export const Navigation = () => {
 
         <Route element={<AuthLayout />}>
           <Route element={<SignInPage />} path={RoutesPath.signIn} />
+          <Route element={<SignUpPage />} path={RoutesPath.signUp} />
         </Route>
       </Routes>
     </BrowserRouter>
