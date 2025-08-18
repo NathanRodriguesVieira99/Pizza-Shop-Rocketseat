75a5c0fa4b7ba05008d2aa7c620891e7c22575cf
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-18 20:45:32 -0300

    feat(auth): implement toast notifications for SignIn

--- src/pages/auth/Sign-In/SignInModel.ts
+++ src/pages/auth/Sign-In/SignInModel.ts
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 import type { SignFormSchema } from './SignIn.schema';
 
 export const useSignInModel = () => {
@@ -9,9 +10,18 @@ export const useSignInModel = () => {
   } = useForm<SignFormSchema>();
 
   const onSubmit = async (data: SignFormSchema) => {
-    console.log(data);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success('Enviamos um link de autenticação para o seu e-mail', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => onSubmit(data),
+        },
+      });
+    } catch {
+      toast.error('Credenciais inválidas');
+    }
   };
 
   const handleSignIn = handleSubmit(onSubmit);
