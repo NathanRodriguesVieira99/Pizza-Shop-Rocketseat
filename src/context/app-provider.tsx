0ada7fed8ff2992149b1ce1d6348bbf000a93c72
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-20 22:35:58 -0300

    feat(provider):  add ThemeProvider to AppProvider

--- src/context/app-provider.tsx
+++ src/context/app-provider.tsx
@@ -1,13 +1,16 @@
 import type { ReactNode } from 'react';
 import { QueryClientProviders } from './providers/query-client-provider';
 import { ReactHelmetProvider } from './providers/react-helmet-async-provider';
+import { ThemeProvider } from './providers/theme/theme-provider';
 import { ToastProvider } from './providers/toast-provider';
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProviders>
       <ReactHelmetProvider>
-        <ToastProvider>{children}</ToastProvider>
+        <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+          <ToastProvider>{children}</ToastProvider>
+        </ThemeProvider>
       </ReactHelmetProvider>
     </QueryClientProviders>
   );
