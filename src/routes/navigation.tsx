acd3bcd1ea24806c720b24a6d98d2f1efbd151a1
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-20 22:29:26 -0300

    feat(navigation): add Orders route to navigation

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import { AppLayout } from '@/pages/_layouts/app';
 import { AuthLayout } from '@/pages/_layouts/auth';
 import { DashboardPage } from '@/pages/app/Dashboard/DashboardPage';
+import { OrdersPage } from '@/pages/app/Orders/OrdersPage';
 import { SignInPage } from '@/pages/auth/Sign-In/SignInPage';
 import { SignUpPage } from '@/pages/auth/SignUp/SignUpPage';
 
 export const RoutesPath = {
   signIn: '/signIn',
   signUp: '/signUp',
+  Orders: '/orders',
 } as const;
 
 export const Navigation = () => {
@@ -16,6 +18,7 @@ export const Navigation = () => {
       <Routes>
         <Route element={<AppLayout />}>
           <Route element={<DashboardPage />} index />
+          <Route element={<OrdersPage />} path={RoutesPath.Orders} />
         </Route>
 
         <Route element={<AuthLayout />}>
