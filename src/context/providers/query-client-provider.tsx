9d4fbb7c5ef6be3c51a7dbb85be4d75bc444e217
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-21 20:56:11 -0300

    feat(react-query): move QueryClient instance to a separate file

--- src/context/providers/query-client-provider.tsx
+++ src/context/providers/query-client-provider.tsx
@@ -1,7 +1,6 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
-
-export const client = new QueryClient();
+import { client } from '@/lib/react-query';
 
 export const QueryClientProviders = ({ children }: { children: ReactNode }) => {
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
