04c684a8bdbc3c6e8ca3e14e6821f95a31e62997
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-20 22:32:51 -0300

    feat(config): add VITE_API_BASE_URL to env schema

--- src/config/env/config.ts
+++ src/config/env/config.ts
@@ -2,13 +2,13 @@ import { treeifyError, z } from 'zod';
 
 const envSchema = z.object({
   VITE_ENV: z.enum(['test', 'e2e', 'development', 'production']),
+  VITE_API_BASE_URL: z.url(),
 });
 
 const _env = envSchema.safeParse(import.meta.env);
 
 if (_env.success === false) {
   if (import.meta.env.VITE_ENV !== 'production') {
-    // biome-ignore lint/suspicious/noConsole: necessary to catch env errors
     console.error('❌ Invalid environment variable', treeifyError(_env.error));
   }
   throw new Error('❌ Invalid environment variable');
