70e98847624291cf5b12d710dfa55b462d0d8d5c
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-21 21:04:18 -0300

    feat(OrdersView): refactor OrdersView to use OrderTableFilter and simplify table structure

--- src/pages/app/Orders/OrdersView.tsx
+++ src/pages/app/Orders/OrdersView.tsx
@@ -1,16 +1,15 @@
 /** biome-ignore-all lint/suspicious/noArrayIndexKey: is necessary by now use index on array */
-import { ArrowRight, Search, X } from 'lucide-react';
 import { Helmet } from 'react-helmet-async';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { Pagination } from '@/components/layout/pagination';
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { OrderTableFilter } from './_components/order-table-filter';
+import { OrderTableRow } from './_components/order-table-row';
 // import type { useOrdersModel } from './OrdersModel';
 
 // type OrdersViewProps = ReturnType<typeof useOrdersModel>;
@@ -22,79 +21,32 @@ export const OrdersView = () => {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="font-bold text-3xl tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <form className="flex items-center gap-2 ">
-          <span className="font-semibold text-sm">Filtros</span>
-          <Input className="h-8 w-[320px]" placeholder="Nome do cliente" />
-        </form>
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]" />
-                <TableHead className="w-[140px]">Identificador</TableHead>
-                <TableHead className="w-[180px]">Realizado há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead className="">Cliente</TableHead>
-                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                <TableHead className="w-[164px]" />
-                <TableHead className="w-[132px]" />
-              </TableRow>
-            </TableHeader>
 
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <TableRow key={i}>
-                    <TableCell>
-                      <Button size={'xs'} variant={'outline'}>
-                        <Search className="h-3 w-3" />
-                        <span className="sr-only">Detalhes do pedido</span>
-                      </Button>
-                    </TableCell>
+        <div className="space-x-2.5 space-y-2.5">
+          <OrderTableFilter />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]" />
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead className="">Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]" />
+                  <TableHead className="w-[132px]" />
+                </TableRow>
+              </TableHeader>
 
-                    <TableCell className="font-medium font-mono text-xs">
-                      394=e09rdfojf490djdfh03-rier
-                    </TableCell>
-
-                    <TableCell className="text-muted-foreground">
-                      Há 15 minutos
-                    </TableCell>
-
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400" />
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-
-                    <TableCell className="font-medium">
-                      Nathan Rodrigues Vieira
-                    </TableCell>
-
-                    <TableCell className="font-medium">R$ 59,00</TableCell>
-
-                    <TableCell>
-                      <Button size="sm" variant={'outline'}>
-                        <ArrowRight className="mr-2 h-3 w-3" />
-                        Aprovar
-                      </Button>
-                    </TableCell>
-
-                    <TableCell>
-                      <Button size="sm" variant="ghost">
-                        <X className="mr-2 h-3 w-3" />
-                        Cancelar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />;
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} perPage={10} totalCount={105} />
         </div>
       </div>
     </>
