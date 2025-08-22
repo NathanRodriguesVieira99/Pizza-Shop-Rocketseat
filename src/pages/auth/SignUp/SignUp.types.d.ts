7ae4d5f51c09a2933e22045354cf44c9e59c08e7
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-22 19:53:08 -0300

    feat(SignUp): add RegisterRestaurantProps type

---
+++ src/pages/auth/SignUp/SignUp.types.d.ts
@@ -0,0 +1,5 @@
+import type { IRegisterRestaurantService } from '@/services/api/register-restaurant-service/register-restaurant-service';
+
+export type RegisterRestaurantProps = {
+  RegisterRestaurantService: IRegisterRestaurantService;
+};
