d39e3b4835364301ce2db8cd7c412e02cdff18d5
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-21 21:08:47 -0300

    feat(DashboardView): enhance layout and structure of the dashboard

--- src/pages/app/Dashboard/DashboardView.tsx
+++ src/pages/app/Dashboard/DashboardView.tsx
@@ -1,4 +1,10 @@
 import { Helmet } from 'react-helmet-async';
+import { DayOrdersAmountCard } from './_components/day-orders-amount-card';
+import { MonthCanceledOrdersAmountCard } from './_components/month-canceled-orders-amount-card';
+import { MonthOrdersAmountCard } from './_components/month-orders-amount-card';
+import { MonthRevenueCard } from './_components/month-revenue-card';
+import { PopularOrdersChart } from './_components/popular-orders-chart';
+import { RevenueChart } from './_components/revenue-chart';
 import type { useDashboardModel } from './DashboardModel';
 
 type DashboardViewProps = ReturnType<typeof useDashboardModel>;
@@ -9,7 +15,21 @@ export const DashboardView = (props: DashboardViewProps) => {
   return (
     <>
       <Helmet title="Dashboard" />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="font-bold text-3xl tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
+
+        <div className=" grid grid-cols-9 gap-4 ">
+          <RevenueChart />
+          <PopularOrdersChart />
+        </div>
+      </div>
     </>
   );
 };
