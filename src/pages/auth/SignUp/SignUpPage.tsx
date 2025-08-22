d6e6de8ec6c6b976665dfe26aff10afd5402b715
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-22 19:56:14 -0300

    feat(SignUpPage): integrate RegisterRestaurantService into useSignUpModel

--- src/pages/auth/SignUp/SignUpPage.tsx
+++ src/pages/auth/SignUp/SignUpPage.tsx
@@ -1,8 +1,12 @@
+import { HttpClient } from '@/infra/http/HttpClient';
+import { RegisterRestaurantService as ServiceRegisterRestaurant } from '@/services/api/register-restaurant-service/register-restaurant-service';
 import { useSignUpModel } from './SignUpModel';
 import { SignUpView } from './SignUpView';
 
 export const SignUpPage = () => {
-  const methods = useSignUpModel();
+  const httpClient = HttpClient.create();
+  const RegisterRestaurantService = new ServiceRegisterRestaurant(httpClient);
+  const methods = useSignUpModel({ RegisterRestaurantService });
 
   return <SignUpView {...methods} />;
 };
