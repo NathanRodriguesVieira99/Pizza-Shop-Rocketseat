5978a5a13860ebf7f0bc4a93a9bddb8cffc56dc4
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-23 12:11:49 -0300

    refactor(Orders): add comment to indicate where props should be passed

--- src/pages/app/Orders/OrdersView.tsx
+++ src/pages/app/Orders/OrdersView.tsx
@@ -14,6 +14,7 @@ import { OrderTableRow } from './_components/order-table-row';
 
 // type OrdersViewProps = ReturnType<typeof useOrdersModel>;
 
+// passar props quando necessário
 export const OrdersView = () => {
   // const {} = props;
   return (
