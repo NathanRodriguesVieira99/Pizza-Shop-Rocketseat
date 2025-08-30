e3763d515ca489cc5f9f2bc732878b4b763bf84f
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-29 22:27:13 -0300

    feat(AppLayout): add Axios interceptor for handling unauthorized responses

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,7 +1,33 @@
-import { Outlet } from 'react-router';
+import { isAxiosError } from 'axios';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router';
 import { Header } from '@/components/layout/header';
+import { HttpClient } from '@/infra/http/HttpClient';
 
 export const AppLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const httpClient = HttpClient.create();
+    const interceptorId = httpClient.Instance.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status;
+          const code = error.response?.data.code;
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/signin', { replace: true });
+          }
+        }
+      }
+    );
+
+    return () => {
+      httpClient.Instance.interceptors.request.eject(interceptorId);
+    };
+  }, [navigate]);
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
