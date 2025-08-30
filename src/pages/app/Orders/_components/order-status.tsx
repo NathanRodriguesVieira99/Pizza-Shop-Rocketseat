b88cdde0aa942e2daecdea1ffa9ba9b7952d0d5f
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-29 22:17:24 -0300

    feat(order-status): add OrderStatus component to display order status with visual indicators

---
+++ src/pages/app/Orders/_components/order-status.tsx
@@ -0,0 +1,36 @@
+import type {
+  OrderStatusProps,
+  OrderStatus as StatusOrder,
+} from '../orders-types';
+
+export const OrderStatus = ({ status }: OrderStatusProps) => {
+  const orderStatusMap: Record<StatusOrder, string> = {
+    pending: 'Pendente',
+    canceled: 'Cancelado',
+    delivered: 'Entregue',
+    delivering: 'Em entrega',
+    processing: 'Em preparo',
+  };
+
+  return (
+    <div>
+      <div className="flex items-center gap-2">
+        {status === 'pending' && (
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+        )}
+        {status === 'canceled' && (
+          <span className="h-2 w-2 rounded-full bg-rose-500" />
+        )}
+        {status === 'delivered' && (
+          <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        )}
+        {['processing', 'delivering'].includes(status) && (
+          <span className="h-2 w-2 rounded-full bg-amber-500" />
+        )}
+        <span className="font-medium text-muted-foreground">
+          {orderStatusMap[status]}
+        </span>
+      </div>
+    </div>
+  );
+};
