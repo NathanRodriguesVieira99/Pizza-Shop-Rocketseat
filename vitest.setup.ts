b18bf2fdb7a5c6912c8c5c337d86519c3dd014af
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-31 15:24:56 -0300

    fix(vitest): add MSW  cleanups

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,11 +1,24 @@
 import { cleanup } from '@testing-library/react';
-import { afterEach, vi } from 'vitest';
+import { setupServer } from 'msw/node';
+import { afterAll, afterEach, beforeAll, vi } from 'vitest';
+import { handlers } from './src/services/api/mocks/handlers';
+
+const server = setupServer(...handlers);
 
 // Essa função roda automaticamente depois de cada teste
 // Serve pra limpar tudo e evitar que um teste interfira no outro
 afterEach(() => {
   // Limpa o DOM entre os testes (remove o que foi renderizado)
   cleanup();
+  
+  // Start server before all tests
+  beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+  // Close server after all tests
+  afterAll(() => server.close());
+
+  // Reset handlers after each test for test isolation
+  afterEach(() => server.resetHandlers());
 
   // Reseta todos os spies e mocks do Vitest (`vi.fn`, `vi.spyOn`, etc.)
   // Garante que os testes sejam independentes e não tenham "lixo" de execuções anteriores
