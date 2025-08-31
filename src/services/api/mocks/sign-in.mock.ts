441d55d82d16abd990bf32e7ac1578a6f997eed8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-31 19:20:17 -0300

    feat(mocks): add mock  for user sign-in authentication

---
+++ src/services/api/mocks/sign-in.mock.ts
@@ -0,0 +1,20 @@
+import { HttpResponse, http } from 'msw';
+import type { SignInBody } from '../sign-in';
+
+export const SignInMock = http.post<never, SignInBody>(
+  '/authenticate',
+  async ({ request }) => {
+    const { email } = await request.json();
+
+    if (email === 'jhondoe@example.com') {
+      return new HttpResponse(null, {
+        status: 200,
+        headers: {
+          'Set-Cookie': 'auth-sample-jwt',
+        },
+      });
+    }
+
+    return new HttpResponse(null, { status: 401 });
+  }
+);
